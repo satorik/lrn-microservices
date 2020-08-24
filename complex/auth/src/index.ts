b3b505b83fc67a3b75d5e3ffd4e338f982561cde
satorik <satorik@yandex.ru>
2020-08-24 19:09:41 +0300

    auth service completed

--- complex/auth/src/index.ts
+++ complex/auth/src/index.ts
@@ -1,7 +1,21 @@
-import express from 'express'
-import { json } from 'body-parser'
+import mongoose from 'mongoose'
+import { app } from './app'
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined!')
+  }
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    console.log('connected to MongoDB!')
+  } catch (err) {
+    console.log(err)
+  }
 
-const app = express()
-app.use(json())
+  app.listen(3000, () => console.log('Listening on port 3000'))
+}
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+start()
---
+++ complex/auth/src/routes/signin.ts
@@ -0,0 +1,52 @@
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
+import { validateRequest } from '../middlewares/validate-request'
+import jwt from 'jsonwebtoken'
+
+import { User } from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error'
+import { Password } from '../services/password'
+
+const router = express.Router()
+
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials')
+    }
+
+    const isMatch = await Password.compare(existingUser.password, password)
+    if (!isMatch) {
+      throw new BadRequestError('Invalid credentials')
+    }
+
+    //jwt
+    const userJWT = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    )
+
+    req.session = {
+      jwt: userJWT,
+    }
+
+    res.status(200).send(existingUser)
+  }
+)
+
+export { router as signinRouter }
