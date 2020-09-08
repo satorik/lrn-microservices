d12434aaacb3945db57eb223dc24698321bf1063
satorik <satorik@yandex.ru>
2020-09-08 14:33:56 +0300

    preFinal version

--- complex/client/components/header.js
+++ complex/client/components/header.js
@@ -3,6 +3,8 @@ export default ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign up', href: '/auth/signup' },
     !currentUser && { label: 'Sign in', href: '/auth/signin' },
+    currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
+    currentUser && { label: 'My orders', href: '/orders/' },
     currentUser && { label: 'Sign out', href: '/auth/signout' },
   ]
     .filter((linkConfig) => linkConfig)
