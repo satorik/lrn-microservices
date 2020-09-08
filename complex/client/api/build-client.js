43b80a8fbfab0423133f1751ff7d3f0738cd5154
satorik <satorik@yandex.ru>
2020-09-09 00:40:28 +0300

    bug fixes for DO

--- complex/client/api/build-client.js
+++ complex/client/api/build-client.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'www.k8s.satorik.ru',
       headers: req.headers,
     })
   } else {
